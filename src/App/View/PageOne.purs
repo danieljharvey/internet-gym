f58ff128e1f50f5b8787254662894cb6bbc4273b
Daniel Harvey <danieljamesharvey@gmail.com>
2018-07-08 17:47:43 +0100

    Some styles of a sort

--- src/App/View/PageOne.purs
+++ src/App/View/PageOne.purs
@@ -1,19 +1,24 @@
 module App.View.PageOne where
 
-import Prelude
-import App.PageOne.Events (PageOneEvent(..))
+import Prelude (($), discard, show)
 import App.Events (Event(..))
+import App.PageOne.Events (PageOneEvent(..))
 import App.State (State(..))
 import Pux.DOM.Events (onSubmit, onChange)
 import Pux.DOM.HTML (HTML)
-import Text.Smolder.HTML (button, form, input)
-import Text.Smolder.HTML.Attributes (name, type', value)
+import Text.Smolder.HTML (button, form, input, label, div)
+import Text.Smolder.HTML.Attributes (className, name, type', value)
 import Text.Smolder.Markup ((!), (#!), text)
 
 view :: State -> HTML Event
 view (State st) =
-  form ! name "signin" #! onSubmit (\ev -> PageOne (SignIn ev)) $ do
-    input ! type' "text" ! value st.pageOne.name #! onChange (\ev -> PageOne (ChangeName ev))
-    input ! type' "text" ! value (show st.pageOne.age) #! onChange (\ev -> PageOne (ChangeAge ev))
-    button ! type' "submit" $ text "Sign In"
+  form ! name "signin" ! className "pageOne" #! onSubmit (\ev -> PageOne (SignIn ev)) $ do
+    div ! className "formSection" $ do
+      label do text "Name"
+      input ! type' "text" ! value st.pageOne.name #! onChange (\ev -> PageOne (ChangeName ev))
+    div ! className "formSection" $ do
+      label do text "Age"
+      input ! type' "text" ! value (show st.pageOne.age) #! onChange (\ev -> PageOne (ChangeAge ev))
+    div ! className "formSection" $ do
+      button ! type' "submit" $ text "Sign In"
 
