5f4d75214f2fb7eb3f041a4e168751cb44ec28e9
Daniel Harvey <danieljamesharvey@gmail.com>
2018-07-14 12:55:22 +0100

    Better validation

--- src/App/Data/PageOneValidation.purs
+++ src/App/Data/PageOneValidation.purs
@@ -1,24 +1,32 @@
 module App.PageOne.Validation where
 
-import Prelude ((==), compare, Ordering(GT), (=<<), (>))
 import App.State (PageOneState)
 import Data.Either (Either(..))
-import Data.String.CodeUnits (length)
+import Data.List (List(..), elem)
+import Prelude ((==), compare, Ordering(GT), (=<<), (<>), ($), not)
 
 validateState :: PageOneState -> Either String PageOneState
-validateState state = isLikesDogs =<< isOverTen state
+validateState state = isLikesDogs =<< isOverTen =<< badFirstName =<< badLastName state
+
+validator :: forall a. (a -> Boolean) -> (a -> String) -> a -> Either String a
+validator predicate message state = case (predicate state) of
+    true  -> Right state
+    false -> Left $ message state
 
 isLikesDogs :: PageOneState -> Either String PageOneState
-isLikesDogs state = case (state.likesDogs == false) of
-    true  -> Right state
-    false -> Left "No dog love please, this is a place of hygiene"
+isLikesDogs = validator (\s -> s.likesDogs == false) (\s -> "No dog love please, this is a place of hygiene")
 
 isOverTen :: PageOneState -> Either String PageOneState
-isOverTen state = case (compare state.age 10) == GT of 
-    true  -> Right state
-    false -> Left "Absolutely too young for this damn gym"
+isOverTen = validator (\s -> (compare s.age 10) == GT) (\s -> "Absolutely too young for this damn gym")
 
-noFirstName :: PageOneState -> Either String PageOneState
-noFirstName state = case (length state.firstName) > 3 of 
-    true  -> Right state
-    false -> Left "First name is too short"
+badFirstName :: PageOneState -> Either String PageOneState
+badFirstName = validator (\s -> not $ elem s.firstName badFirstNames) (\s -> "I'm afraid we don't allow " <> s.firstName <> "s in here.")
+
+badLastName :: PageOneState -> Either String PageOneState
+badLastName = validator (\s -> not $ elem s.lastName badLastNames) (\s -> "I'm afraid we don't like the " <> s.lastName <> " family around here.")
+
+badFirstNames :: List String
+badFirstNames = Cons "Bruce" $ Cons "Steve" $ Cons "Horse" $ Cons "Brad" $ Nil
+
+badLastNames :: List String
+badLastNames = Cons "Smith" $ Cons "Jones" $ Cons "Cleese" $ Cons "Sleaze" $ Nil
