e21914e350ab5f92eb4a7303e82677696b36c5d7
Daniel J Harvey <dan.harvey@futurice.com>
2018-08-24 22:36:03 +0100

    Somewhat OK API call

--- src/App/Events.purs
+++ src/App/Events.purs
@@ -1,31 +1,34 @@
 module App.Events where
 
+import App.Dog (Dog)
+
 import App.PageOne.Events (PageOneEvent)
 import App.PageOne.Reducer as P1Reducer
 import App.Routes (Route, match)
 import App.State (State(..))
-import Effect.Aff (attempt)
+
 import Control.Applicative (pure)
 import Control.Bind ((=<<), bind)
-import Data.Argonaut (class DecodeJson, decodeJson, (.?))
+import Data.Either (Either(..))
 import Data.Function (($))
 import Data.Maybe (Maybe(..))
-import Data.Either (Either(Left, Right), either)
-import Network.HTTP.Affjax (get)
-import Prelude (discard, (<>), show)
 import Effect.Class (liftEffect)
+import Foreign (unsafeToForeign)
+import Prelude (discard, (<>), show)
+import Pux (EffModel, noEffects, onlyEffects)
+import Pux.DOM.Events (DOMEvent)
 import Web.Event.Event (preventDefault)
 import Web.HTML (window)
 import Web.HTML.History (DocumentTitle(..), URL(..), pushState)
 import Web.HTML.Window (history)
-import Foreign (unsafeToForeign)
-import Data.Function (($))
-import Data.Maybe (Maybe(..))
-import Prelude (discard)
-import Pux (EffModel, noEffects, onlyEffects)
-import Pux.DOM.Events (DOMEvent)
-import App.Dog
-import Data.Function (($), (<<<), const)
+
+
+import Data.Argonaut.Core as J
+
+import Data.HTTP.Method (Method(..))
+import Effect.Console (log)
+import Network.HTTP.Affjax as AX
+import Network.HTTP.Affjax.Response as AXRes
 
 data Event
   = PageView Route
@@ -34,6 +37,10 @@ data Event
   | RequestDogs
   | ReceiveDogs (Either String Dog)
   
+apiPath :: String
+apiPath = "https://dog.ceo/api/breeds/image/random"
+
+
 foldp :: Event -> State -> EffModel State Event
 foldp (PageView route) (State st) = noEffects $ State st { route = route, loaded = true }
 foldp (Navigate url ev) (State st) = onlyEffects (State st) [
@@ -52,9 +59,12 @@ foldp (ReceiveDogs (Right dogs)) (State st) =
 foldp (RequestDogs) (State st) =
   { state: State st { dogs = { dogs: [], status: "Fetching todos..." } }
   , effects: [ do
-      res <- attempt $ get "https://dog.ceo/api/breeds/image/random"
-      let decode r = decodeJson r.response :: Either String Dog
-      let dogs = either (Left <<< show) decode res
-      pure $ Just $ ReceiveDogs dogs
+      res <- AX.affjax AXRes.json (AX.defaultRequest { url = "/api", method = Left GET })
+      liftEffect $ log $ "GET /api response: " <> J.stringify res.response
+      pure Nothing
     ]
   }
+
+  -- let decode r = decodeJson r.response :: Either String Dog
+      -- let dogs = either (Left <<< show) decode res
+      -- pure $ Just $ ReceiveDogs dogs
