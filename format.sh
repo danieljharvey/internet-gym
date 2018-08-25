f9ca1eba8a44395263f9232afd51abcccdc01fe2
Daniel J Harvey <dan.harvey@futurice.com>
2018-08-25 16:32:46 +0100

    Format tests

--- format.sh
+++ format.sh
@@ -4,4 +4,6 @@ echo "Formatting..."
 
 find "./src" -type f -iname *.purs -exec purty --write --dynamic '{}' \;
 
+find "./test" -type f -iname *.purs -exec purty --write --dynamic '{}' \;
+
 echo "Done!"
